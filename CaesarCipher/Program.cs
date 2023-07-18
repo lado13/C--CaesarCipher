f8e036472bc23d6fb32257a903b9385b05974e6d
lado13 <119516940+lado13@users.noreply.github.com>
2023-07-18 19:50:59 +0400

    Add project files.

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CaesarCipher
+{
+    internal class Program
+    {
+
+
+        public static string Encrypt(string message, int shift)
+        {
+
+            char[] characters = message.ToCharArray();
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                characters[i] = (char)(characters[i] + shift);
+            }
+
+            Console.WriteLine(characters);
+            return characters.ToString();
+        }
+
+
+        public static string Decrypt(string message, int shift)
+        {
+
+            int decryptShift = -shift;
+            return Encrypt(message, decryptShift);
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+            Encrypt("Hello", 2);
+            Decrypt("Jgnnq",2);
+        }
+    }
+}
